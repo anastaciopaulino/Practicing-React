8b1ca4f9f3b270f61f72f215b605d29fdf823ba4
anastaciopaulino <anastaciopaulino008@gmail.com>
2022-01-06 21:57:29 +0100

    Building TaskList component.

---
+++ To do List/src/components/User/TaskList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "TaskList.module.css";
+
+const TaskList = (props) => {
+  return (
+    <>
+      <div className={container}></div>
+    </>
+  );
+};
