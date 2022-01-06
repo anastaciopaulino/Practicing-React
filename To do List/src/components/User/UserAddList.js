6ccd6a4a4a3dd60f135b9d256c23c87177e15e70
anastaciopaulino <anastaciopaulino008@gmail.com>
2022-01-06 21:50:53 +0100

    Get the user data.

--- To do List/src/components/User/UserAddList.js
+++ To do List/src/components/User/UserAddList.js
@@ -4,10 +4,23 @@ import classes from "./UserAddList.module.css";
 import Card from "../UI/Card";
 
 const UserAddList = (props) => {
+  const [task, setTask] = useState('')
+
+  const userAddHandler = (event) => {
+    event.preventDefault()
+    console.log(task)
+
+    setTask('')
+  }
+
+  const changeTask = (event) => {
+    setTask(event.target.value)
+  }
+
   return (
     <div>
-      <form action="" className={classes.form}>
-        <input type="text" placeholder="Enter the talk" />
+      <form action="" className={classes.form} onSubmit={userAddHandler}>
+        <input type="text" placeholder="Enter the talk" value={task} onChange={changeTask} />
         <button className={classes.btnSend}>Add</button>
       </form>
     </div>
